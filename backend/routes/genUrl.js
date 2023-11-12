b38c81f0fb20aeacf971578948582bef1e8b92a7
Syed Fazil <syedfazil539@gmail.com>
2023-11-12 15:06:31 +0530

    Ready for deployment

--- backend/routes/genUrl.js
+++ backend/routes/genUrl.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const validUrl = require("valid-url");
 const shortid = require("shortid");
-
+const dotenv = require("dotenv").config();
 const Url = require("../models/Url");
 
 router.post("/", async (request, response) => {
